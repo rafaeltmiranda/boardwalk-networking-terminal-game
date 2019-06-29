f142d65cab1d78885908ebc5fefcfafdce1b9b17
Rui Magalhães <ruidpm2@gmail.com>
2019-06-29 12:43:25 +0100

    Player construtor, addPoints(), subtractPoints() and setAlias() done

--- src/org/academiadecodigo/whiledlings/boardwalk/game/Player.java
+++ src/org/academiadecodigo/whiledlings/boardwalk/game/Player.java
@@ -2,6 +2,8 @@ package org.academiadecodigo.whiledlings.boardwalk.game;
 
 import org.academiadecodigo.bootcamp.Prompt;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class Player {
@@ -12,7 +14,35 @@ public class Player {
     private Prompt prompt;
 
 
-    public void addPoints(int points){}
+    public Player(Socket socket){
 
-    public void subtractPoints(int points){}
+        this.socket = socket;
+        points = 100;
+
+        try {
+            prompt = new Prompt(socket.getInputStream(), new PrintStream(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    void addPoints(int points){
+
+        this.points += points;
+    }
+
+
+    void subtractPoints(int points){
+
+        if ((this.points -= points) < 0){
+           this.points = 0;
+        }
+    }
+
+
+    void setAlias(String alias){
+
+        this.alias = alias;
+    }
 }
--- src/org/academiadecodigo/whiledlings/boardwalk/utility/OutputBuilder.java
+++ src/org/academiadecodigo/whiledlings/boardwalk/utility/OutputBuilder.java
@@ -1,7 +1,7 @@
 package org.academiadecodigo.whiledlings.boardwalk.utility;
 
 public class OutputBuilder {
-    
+
     public static String buildOutput(/*recebe i dunno*/){
 
         return null;
