3374bfa53a8fcd84242c6831386da38889ab296a
Rui Magalhães <ruidpm2@gmail.com>
2019-06-29 11:58:59 +0100

    Add utility class OutputWriter

---
+++ src/org/academiadecodigo/whiledlings/boardwalk/utility/OutputBuilder.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.whiledlings.boardwalk.utility;
+
+public class OutputBuilder {
+    
+    public static String buildOutput(/*recebe i dunno*/){
+
+        return null;
+    }
+}
