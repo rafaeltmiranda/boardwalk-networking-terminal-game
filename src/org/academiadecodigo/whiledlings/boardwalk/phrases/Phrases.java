392214785eb8d1ede5d769fe3a62314fae714d34
Rui Magalhães <ruidpm2@gmail.com>
2019-06-29 13:01:17 +0100

    Create get method and contructor on Phrases Enum

--- src/org/academiadecodigo/whiledlings/boardwalk/phrases/Phrases.java
+++ src/org/academiadecodigo/whiledlings/boardwalk/phrases/Phrases.java
@@ -1,4 +1,22 @@
 package org.academiadecodigo.whiledlings.boardwalk.phrases;
 
 public enum Phrases {
+
+    ENCAPSULATION("ENCAPSULATION CAN BE USED TO HIDE DATA MEMBERS AND MEMBER FUNCTIONS");
+
+
+    private String phrase;
+
+    Phrases(String phrase){
+        this.phrase = phrase;
+    }
+
+
+    public char[] getPhraseAsCharArray(){
+
+        char[] phraseArray = new char[this.phrase.length()];
+        phraseArray = this.phrase.toCharArray();
+
+        return phraseArray;
+    }
 }
