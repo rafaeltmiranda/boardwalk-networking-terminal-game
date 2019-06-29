7a0b1b527a7f5831bc9184791afe7e733690245d
David Ferreira <davidrsferreira@gmail.com>
2019-06-29 15:48:15 +0100

    Create constructor on Room

--- src/org/academiadecodigo/whiledlings/boardwalk/game/Room.java
+++ src/org/academiadecodigo/whiledlings/boardwalk/game/Room.java
@@ -1,19 +1,34 @@
 package org.academiadecodigo.whiledlings.boardwalk.game;
 
-import org.academiadecodigo.whiledlings.boardwalk.game.Player;
-
 import java.util.ArrayList;
 import java.util.Set;
 
 public class Room {
 
+    public static int MAX_PLAYERS = 5;
+
     private ArrayList<Player> players;
     private char[] completePhrase;
     private char[] phrase;
     private Set<Character> alreadyChosen;
+    private String name;
+    private Player roomOwner;
 
+    public Room (String name, Player player){
+        this.name = name;
+        this.roomOwner = player;
+        players.add(player);
+    }
 
-    public void joinRoom(){}
+    public void joinRoom(){
 
-    private void getRandomPhrase(){}
+    }
+
+    private void getRandomPhrase(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
